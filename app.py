52b4d39a186786cebb1558113c1b340d9187463e
Chiragasourabh <Chiragasourabh@gmail.com>
2018-11-07 14:31:59 +0530

    added post method to search from web

--- app.py
+++ app.py
@@ -1,32 +1,44 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 import wikipedia as wp
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('home.html')
-	
+    return render_template('home.html', answer = "Hi there ....")
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    text = request.form['search']
+    return wp.summary(text,sentences=2)
+
+@app.route('/home', methods=['POST'])
+def home_my_form_post():
+    text = request.form['search']
+    return render_template('home.html',answer = wp.summary(text, sentences = 2))
+
 @app.route('/home')
 def home():
-    return render_template('home.html')
-	
+    return render_template('home.html', answer = "Hi there ....")
+
 @app.route('/about')
 def about():
     return render_template('about.html')
-	
+
 @app.route('/howitworks')
 def howitworks():
     return render_template('howitworks.html')
-	
+
 @app.route('/contact')
 def contact():
     return render_template('404.html')
-	
+
 @app.route('/auth')
 def auth():
     return render_template('404.html')
-	
+
+
+
 @app.route('/wiki')
 def wikitest():
 	return wp.summary("who invented facebook?", sentences=2)

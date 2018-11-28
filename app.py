179e0b4cb19e95bd4e751191774a08bcf8feaa04
Chiragasourabh <Chiragasourabh@gmail.com>
2018-11-28 15:58:48 +0530

    updated links in about page

--- app.py
+++ app.py
@@ -11,11 +11,6 @@ def index():
 
 @app.route('/', methods=['POST'])
 def my_form_post():
-    '''r=sr.Recognizer()
-    mic = sr.Microphone()
-    with mic as source:
-        audio=r.listen(source)
-    return r.recognize_google(audio)'''
     text = request.form['search']
     #return wp.summary(r.recognize_google(audio),sentences=2)
     return render_template('home.html',answer = wp.summary(text,sentences=2))

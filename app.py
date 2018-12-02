e9e5da78c5ce2ab274191b9e653df2d8ae8ad626
Chiragasourabh <Chiragasourabh@gmail.com>
2018-12-02 21:26:39 +0530

    updated profile , login and dashboard methods

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, render_template, flash, redirect, url_for, session
 import wikipedia as wp
 import config
+from datetime import datetime
+#import thread
 
 app = Flask(__name__)
 
@@ -8,34 +10,55 @@ app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 @app.route('/')
 def index():
-    return render_template('home.html', answer = "Hi there ....")
+    return render_template('home.html', answer = "Hi there ...." , login=config.isLoggedIn())
 
 @app.route('/', methods=['POST'])
 def my_form_post():
     text = request.form['search']
-    #return wp.summary(r.recognize_google(audio),sentences=2)
-    return render_template('home.html',answer = wp.summary(text,sentences=2))
+    try:
+        ans = wp.summary(text,sentences=2)
+        if(config.isLoggedIn()):
+            #thread.start_new_thread(config.historify, (text,request.remote_addr))
+            config.historify(session['id'],text,request.remote_addr)
+            return render_template('home.html',answer=ans , login=True)
+        else:
+            flash("Login to Save History and to get better search results")
+            config.historify("Annonymous",text,request.remote_addr)
+            return render_template('home.html',answer=ans , login=False)
+    except:
+        return render_template('home.html',answer = "Oh Ohh ... Sorry. I couldnt get that . will get that in next version", login=config.isLoggedIn())
+
+
 
 @app.route('/home', methods=['POST'])
 def home_my_form_post():
     text = request.form['search']
-    return render_template('home.html',answer = wp.summary(text, sentences = 2))
+    try:
+        ans = wp.summary(text,sentences=2)
+        if(config.isLoggedIn()):
+            config.historify(text,request.remote_addr)
+            return render_template('home.html',answer=ans , login=True)
+        else:
+            flash("Login to Save History and to get better search results")
+            return render_template('home.html',answer=ans , login=False)
+    except:
+        return render_template('home.html',answer = "Oh Ohh ... Sorry. I couldnt get that . will get that in next version", login=config.isLoggedIn())
 
 @app.route('/home')
 def home():
-    return render_template('home.html', answer = "Hi there ....")
+    return render_template('home.html', answer = "Hi there ....", login=config.isLoggedIn())
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', login=config.isLoggedIn())
 
 @app.route('/howitworks')
 def howitworks():
-    return render_template('howitworks.html')
+    return render_template('howitworks.html', login=config.isLoggedIn())
 
 @app.route('/contact')
 def contact():
-    return render_template('contact.html')
+    return render_template('contact.html', login=config.isLoggedIn())
 
 
 @app.route('/contact', methods=['POST'])
@@ -53,41 +76,52 @@ def contact_form_post():
 
 @app.route('/auth')
 def auth():
-    return render_template('login.html')
+    return render_template('login.html', login=config.isLoggedIn())
 
 
 @app.route('/auth', methods=['POST'])
 def login_post():
     email = request.form['txtEmail']
     password = request.form['txtPassword']
-    #try:
-    #    u=config.signin_with_email_and_password(email,password)
-    #except:
-    session['username'] = email
-    flash(session['username'],"danger")
-    return redirect(url_for('auth'))
+    if(config.signin_with_email_and_password(email,password)):
+        return redirect(url_for('profile'))
+    else:
+        flash("Login Failed ! Please Check Your Credentials","warning")
+        return redirect(url_for('auth'))
     #return u
 
 
 @app.route('/register')
 def Register():
-    return render_template('register.html')
+    return render_template('register.html', login=config.isLoggedIn())
 
 @app.route('/register', methods=['POST'])
 def register_post():
     name = request.form['username']
+    phone = request.form['txtMobile']
     email = request.form['txtEmail']
     password = request.form['password1']
-    return str(config.register_with_email_and_password(email,password))
+    if(config.register_with_email_and_password(name,phone,email,password)):
+        flash("Registration Successful. Please Check mailbox to Varify your email and login here ","success")
+        return redirect(url_for('auth'))
+    else:
+        flash("Registration Failed. Account already exist with email id : "+email+"","danger")
+        return redirect(url_for('Register'))
+
 
 @app.route('/forgotpassword')
 def ResetPassword():
-    return render_template('forgotpassword.html')
+    return render_template('forgotpassword.html', login=config.isLoggedIn())
 
 @app.route('/forgotpassword', methods=['POST'])
 def ResetPassword_post():
     email = request.form['txtEmail']
-    return str(config.reset_password_with_email(email))
+    if(config.reset_password_with_email(email)):
+        flash("Password Reset Link has been sent to "+email+".","success")
+        return redirect(url_for('auth'))
+    else:
+        flash("Account Not Found. Please Check Email Address "+email+".","danger")
+        return redirect(url_for('ResetPassword'))
 
 @app.route('/wiki')
 def wikitest():
@@ -95,7 +129,62 @@ def wikitest():
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html')
+    return render_template('404.html', login=config.isLoggedIn())
+
+
+@app.route('/dashboard')
+def dashboard():
+    if config.isLoggedIn():
+        return redirect(url_for('profile'))
+    else:
+        return redirect(url_for('auth'))
+
+
+@app.route('/dashboard/profile')
+def profile():
+    if config.isLoggedIn():
+        return render_template('dashboard/profile.html', login=config.isLoggedIn(),id =session['id'],email =session['username'],name = session['name'],phone = session['phone'],score="#",tag="Beginner" )
+    else:
+        return redirect(url_for('auth'))
+
+@app.route('/dashboard/profile/changepassword')
+def changepassword():
+    if config.isLoggedIn():
+        email = session['username']
+        if(config.reset_password_with_email(email)):
+            flash("Password Reset Link has been sent to "+email+".","success")
+            return redirect(url_for('profile'))
+        else:
+            flash("Account Not Found. Please Check Email Address "+email+".","danger")
+            return redirect(url_for('profile'))
+    else:
+        return redirect(url_for('auth'))
+
+@app.route('/dashboard/history')
+def history():
+    if config.isLoggedIn():
+        jsonobj=config.history().val()
+        return render_template('dashboard/history.html', login=True,json=jsonobj)
+    else:
+        return redirect(url_for('auth'))
+
+@app.route('/dashboard/future')
+def future():
+    if config.isLoggedIn():
+        return render_template('dashboard/future.html', login=True)
+    else:
+        return redirect(url_for('auth'))
+
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    session.pop('id', None)
+    session.pop('name', None)
+    session.pop('phone', None)
+    flash("Logged Out Successfully","info")
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug = True)
--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 import pyrebase
+from flask import session,flash
+from datetime import datetime
 config = {
     "apiKey": "AIzaSyAyMHEsX7egWQYHQzNmKtdy8xGkd1cXBXE",
     "authDomain": "rummage-1.firebaseapp.com",
@@ -12,29 +14,71 @@ firebase =  pyrebase.initialize_app(config)
 auth=firebase.auth()
 db = firebase.database()
 
-def register_with_email_and_password(email,password):
+def register_with_email_and_password(name,phone,email,password):
     try:
         user=auth.create_user_with_email_and_password(email,password)
         auth.send_email_verification(user['idToken'])
-        return auth.get_account_info(user['idToken'])
+        Register(name,email,phone)
+        return True
     except:
-        return "Registration failed"
+        return False
 
 def signin_with_email_and_password(email,password):
     try:
         user=auth.sign_in_with_email_and_password(email,password)
-        return auth.get_account_info(user['idToken'])
+        session['username'] = user['email']
+        session['id'] = user['localId']
+        jsonv = profiledata().val()
+        for key in jsonv:
+            session['name']=jsonv[key]['name']
+            session['phone']=jsonv[key]['phone']
+        return True
     except:
-        return "Login Failed"
+        return False
 
 def reset_password_with_email(email):
+    try:
         auth.send_password_reset_email(email)
+        return True
+    except:
+        return False
 
 def sendMessage(name,phoneNum,email,message):
     data = {
     "name": name ,
-    "phone": phoneNum ,
     "email": email ,
+    "phone": phoneNum ,
     "message": message
     }
     db.child("messages").push(data)
+
+def Register(name,email,phoneNum):
+    data = {
+    "name": name ,
+    "email": email ,
+    "phone": phoneNum ,
+    }
+    db.child("users").child(session['id']).push(data)
+
+def historify(id,query,ip):
+    data = {
+    "query": query ,
+    "time": str(datetime.now()) ,
+    "ip": str(ip) ,
+    }
+    db.child("history").child(id).push(data)
+
+def isLoggedIn():
+    if 'username' in session:
+        return True
+    else:
+        return False
+
+def profiledata():
+    return db.child("users").child(session['id']).get()
+
+def history():
+    try:
+        return db.child("history").child(session['id']).get()
+    except:
+        return False
